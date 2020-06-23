26a998c56cabe5f173d05a9665448c46c0d5654c
Su Anli <sual0001@gmail.com>
2020-06-23 18:02:18 +0800

    refactor(screens/settlement): add map of principles

--- src/screens/settlement/settlement.tsx
+++ src/screens/settlement/settlement.tsx
@@ -4,6 +4,15 @@ import {List} from 'react-native-paper';
 import styled from 'styled-components/native';
 import useSettlement from './hooks';
 
+type PrincipleKey = 'newLife' | 'death' | 'conviction' | 'society';
+
+const PRINCIPLES: {label: string; key: PrincipleKey}[] = [
+  {label: 'New Life', key: 'newLife'},
+  {label: 'Death', key: 'death'},
+  {label: 'Conviction', key: 'conviction'},
+  {label: 'Society', key: 'society'},
+];
+
 const Component = () => {
   const {props, actions} = useSettlement();
 
@@ -12,38 +21,17 @@ const Component = () => {
       <List.Section>
         <List.Item testID="Principles" title="Principles" />
         <Principles>
-          <Gear
-            selected={false}
-            testID="Principle.newLife"
-            width="45%"
-            title={props.newLife}
-            subtitle="New Life"
-            onPress={() => actions.principleSelected('newLife')}
-          />
-          <Gear
-            selected={false}
-            testID="Principle.death"
-            width="45%"
-            title={props.death}
-            subtitle="Death"
-            onPress={() => actions.principleSelected('death')}
-          />
-          <Gear
-            selected={false}
-            testID="Principle.conviction"
-            width="45%"
-            title={props.conviction}
-            subtitle="Conviction"
-            onPress={() => actions.principleSelected('conviction')}
-          />
-          <Gear
-            selected={false}
-            testID="Principle.society"
-            width="45%"
-            title={props.society}
-            subtitle="Society"
-            onPress={() => actions.principleSelected('society')}
-          />
+          {PRINCIPLES.map(principle => (
+            <Gear
+              key={principle.key}
+              selected={false}
+              testID={`Principle.${principle.key}`}
+              width="45%"
+              title={props[principle.key]}
+              subtitle={principle.label}
+              onPress={() => actions.principleSelected(principle.key)}
+            />
+          ))}
         </Principles>
       </List.Section>
     </Screen>
